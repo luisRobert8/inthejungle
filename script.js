db67226367221a94e3d12a715b4926e6b06bd584
luisRobert8 <luischarles814@gmail.com>
2025-04-10 03:44:38 +0100

    google form

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Sidebar Toggle
 function toggleSidebar() {
   const sidebar = document.getElementById('sidebar');
@@ -7,22 +6,7 @@ function toggleSidebar() {
 
 document.querySelectorAll('.audit-btn').forEach(button => {
   button.onclick = function () {
-    // Open a new window with the Google Form URL
-    const newWindow = window.open('', '_blank'); // Open a new blank window
-    newWindow.document.write(`
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Google Form</title>
-      </head>
-      <body>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeX_pM6cansnFn8dwA_AJIE9kMpojDfKepY5_a0N2TPQWuC5A/viewform?embedded=true" 
-                width="1000" height="2150" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-      </body>
-      </html>
-    `);
-    newWindow.document.close(); // Close the document to render it
+    // Directly open the Google Form in a new tab
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSeX_pM6cansnFn8dwA_AJIE9kMpojDfKepY5_a0N2TPQWuC5A/viewform?usp=sf_link', '_blank');
   };
 });
